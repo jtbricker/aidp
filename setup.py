39d29a0dfef240c57f8b46839567f577bdd8ed0a
Justin Bricker <jtbricker@infiniteenergy.com>
2019-06-08 21:17:03 -0400

    Update setup.py and add requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    README = readme_file.read()
+
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
+
 setup(
-    name='aidp',
-    version='0.0.1',
+    name="aidp",
+    version="0.0.1",
+    author="Justin Bricker",
+    author_email="jt.bricker@gmail.com",
+    description="A tool for interacting with AIDP model",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jtbricker/aidp/",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
     entry_points={
         'console_scripts': [
             'aidp=aidp:main'
